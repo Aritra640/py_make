ea2e2e0c83bce3bbb138ad1b14aca08861e8bed7
Aritra640 <67278131+Aritra640@users.noreply.github.com>
2021-05-20 19:35:19 +0530

    Add files via upload

---
+++ snippet.py
@@ -0,0 +1,32 @@
+import sys
+import os
+
+#fast i/o in python 3
+# for normal input :
+def getintegers():
+    return map(int, sys.stdin.readline().strip().split())
+
+def getlist():
+    return list(map(int, sys.stdin.readline().strip().split()))
+
+def get():
+    return sys.stdin.readline().strip()
+
+# only for online judges :
+input = io.BytesIO(os.read(0, \ os.fstat(0).st_size)).readline
+
+#to take string input do this :
+s = input().decode()
+
+n = 5
+sys.stdout.write(str(n)+"\n") # printing integers
+
+# for printing  string 
+s = "Hello World!"
+sys.stdout.write(s)
+
+#for printing a list of integers :
+arr = [1,2,3,4,5]
+sys.stdout.write(
+    " ".join(map(str,arr))+"\n"
+)
